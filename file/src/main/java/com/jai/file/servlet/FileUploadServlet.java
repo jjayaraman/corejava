73807348f72efb5e0c959f7674fe67726476f269
Jayakumar Jayaraman <jayakumar.jayaraman@eba.europa.eu>
2017-10-26 18:05:59 +0100

    file

--- file/src/main/java/com/jai/file/servlet/FileUploadServlet.java
+++ file/src/main/java/com/jai/file/servlet/FileUploadServlet.java
@@ -1,13 +1,9 @@
 package com.jai.file.servlet;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.logging.Level;
+import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -18,72 +14,52 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-@WebServlet(name = "FileUploadServlet", urlPatterns = { "/upload" })
-@MultipartConfig
+import org.apache.commons.io.FileUtils;
+
+@WebServlet(name = "FileUploadServlet", urlPatterns = { "/upload" }, loadOnStartup = 1)
+@MultipartConfig()
 public class FileUploadServlet extends HttpServlet {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 5222389847735084483L;
 
 	private final static Logger LOGGER = Logger.getLogger(FileUploadServlet.class.getCanonicalName());
 
-	
+	// FileDao dao = new FileDao();
+
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.setContentType("text/html;charset=UTF-8");
 
-		// Create path components to save the file
-		final String path = request.getParameter("destination");
-		final Part filePart = request.getPart("file");
-		final String fileName = getFileName(filePart);
+		System.out.println("doPost called ..");
 
-		OutputStream out = null;
-		InputStream filecontent = null;
-		final PrintWriter writer = response.getWriter();
+		// if (ServletFileUpload.isMultipartContent(request)) {
+		// System.out.println("isMultipartContent true ..");
+		// }
+		// else {
+		// System.out.println("isMultipartContent false ..");
+		// }
 
-		try {
-			out = new FileOutputStream(new File(path + File.separator + fileName));
-			filecontent = filePart.getInputStream();
+		// Retrieves <input type="file" name="file">
+		Part filePart = request.getPart("file");
+		System.out.println("filePart :: " + filePart);
+		// MSIE fix
+		String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+		InputStream inputStream = filePart.getInputStream();
+		System.out.println("File size :: " + inputStream.available());
 
-			int read = 0;
-			final byte[] bytes = new byte[1024];
+		File destination = new File(fileName);
+		FileUtils.copyInputStreamToFile(inputStream, destination);
+		System.out.println("file copied..");
 
-			while ((read = filecontent.read(bytes)) != -1) {
-				out.write(bytes, 0, read);
-			}
-			writer.println("New file " + fileName + " created at " + path);
-			LOGGER.log(Level.INFO, "File{0}being uploaded to {1}", new Object[] { fileName, path });
-		} catch (FileNotFoundException fne) {
-			writer.println("You either did not specify a file to upload or are "
-					+ "trying to upload a file to a protected or nonexistent " + "location.");
-			writer.println("<br/> ERROR: " + fne.getMessage());
-
-			LOGGER.log(Level.SEVERE, "Problems during file upload. Error: {0}", new Object[] { fne.getMessage() });
-		} finally {
-			if (out != null) {
-				out.close();
-			}
-			if (filecontent != null) {
-				filecontent.close();
-			}
-			if (writer != null) {
-				writer.close();
-			}
-		}
+		// dao.directInsert(fileName, inputStream);
+		// System.out.println("inserted..");
 	}
 
-	private String getFileName(final Part part) {
-		final String partHeader = part.getHeader("content-disposition");
-		LOGGER.log(Level.INFO, "Part Header = {0}", partHeader);
-		for (String content : part.getHeader("content-disposition").split(";")) {
-			if (content.trim().startsWith("filename")) {
-				return content.substring(content.indexOf('=') + 1).trim().replace("\"", "");
-			}
-		}
-		return null;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doGet called....");
 	}
 
 }
